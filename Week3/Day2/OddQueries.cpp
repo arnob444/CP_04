e37710c22e817de9ded278e3ffb4b774d09c82ed
arnob444 <shacha15-5277@diu.edu.bd>
2024-04-01 01:15:04 +0600

    add

--- Week3/Day2/OddQueries.cpp
+++ Week3/Day2/OddQueries.cpp
@@ -1,42 +1,41 @@
+#pragma GCC optimize("3")
+#pragma GCC target("avx,avx2,fma")
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long int ll;
+void fastIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
 int main()
 {
+    fastIO();
     int t;
     cin >> t;
     while (t--)
     {
         int n, q;
         cin >> n >> q;
-        int arr[n + 5], pre[n + 5], suf[n + 5];
+        vector<ll> arr(n + 1, 0);
         for (int i = 1; i <= n; i++)
         {
             cin >> arr[i];
         }
-        pre[0] = 0;
-        pre[n + 1] = 0;
-        suf[0] = 0;
-        suf[n + 1] = 0;
-        pre[1] = arr[1];
-
-        for (int i = 2; i <= n; i++)
+        // Build prefix sum
+        vector<ll> pref(n + 1, 0);
+        for (int i = 1; i <= n; i++)
         {
-            pre[i] = arr[i] + pre[i - 1];
+            pref[i] = pref[i - 1] + arr[i];
         }
-        suf[n] = arr[n];
-        for (int i = n - 1; i >= 1; i--)
-        {
-            suf[i] = arr[i] + suf[i + 1];
-        }
-
         while (q--)
         {
-            int l, r, k;
+            ll r, l, k;
             cin >> l >> r >> k;
-            int sum = pre[l - 1] + suf[r + 1];
-            int val = r - l + 1;
-            sum += (val * k);
-            if (sum % 2 == 1)
+            ll sum = pref[n]; // Total sum
+            sum -= (pref[r] - pref[l - 1]);
+            sum += ((r - l + 1LL) * k);
+            if (sum % 2LL == 1LL)
                 cout << "YES\n";
             else
                 cout << "NO\n";
