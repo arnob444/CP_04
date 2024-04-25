5c4aad59c47e0769b1b68b1c9fb21c5f3de71305
arnob444 <shacha15-5277@diu.edu.bd>
2024-04-25 20:44:23 +0600

    add

---
+++ Week5/Day7/Bogosort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ll long long int
+#define ON(n, k) (n | (1 << k))
+#define OFF(n, k) (n & (~(1 << k)))
+#define isON(n, k) ((n >> k) & 1)
+#define flip(n, k) ((1 << k) ^ n)
+#define fr for (int i = 0; i < n; i++)
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    sort(v.rbegin(), v.rend());
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
