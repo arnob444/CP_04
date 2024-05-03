d5f9d8d7678b9519f19762bd8f9231c664ae8da8
arnob444 <shacha15-5277@diu.edu.bd>
2024-05-03 23:33:23 +0600

    add

---
+++ Week6/Day7/ChildrenHoliday.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl        "\n"
+#define ll          long long int
+#define ON(n,k)     (n | (1 << k))
+#define OFF(n,k)    (n & (~(1 << k)))
+#define isON(n,k)   ((n >> k) & 1)
+#define flip(n,k)   ((1 << k) ^ n)
+#define gcd(a, b)   __gcd(a, b)
+#define lcm(a, b)   ((a * b) / gcd(a, b))
+#define fr          for (int i = 0; i < n; i++)
+
+void solve()
+{
+    
+}
+int main()
+{
+    return 0;
+}
