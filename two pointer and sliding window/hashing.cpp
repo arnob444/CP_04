94cdafb5976c723a53f896ac32711ee76942a549
arnob444 <shacha15-5277@diu.edu.bd>
2024-07-01 23:53:30 +0600

    add

--- two pointer and sliding window/hashing.cpp
+++ two pointer and sliding window/hashing.cpp
@@ -1,17 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define const N = 1e7;
+int hashing[10000000];
+
 int main()
 {
     int n;
     cin >> n;
     int a[n];
+
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
 
+    // map<int, int> hashing;
     // Pre-compute
-    int hashing[100000000] = {0};
+    // int hashing[10000000] = {0};
+
     for (int i = 0; i < n; i++)
     {
         hashing[a[i]]++;
@@ -20,14 +26,17 @@ int main()
     int q;
     cin >> q;
 
+    // for (auto it : hashing)
+    // {
+    //     cout << it.first << " " << it.second << endl;
+    // }
+    
     while (q--)
     {
         int x;
         cin >> x;
         // Fetch
-
         cout << hashing[a[x]] << endl;
     }
-
     return 0;
 }
---
+++ two pointer and sliding window/hashing_Character.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin >> s;
+
+    // int hashing[26] = {0};
+    // for (int i = 0; i < s.size(); i++)
+    // {
+    //     hashing[s[i] - 'a']++;
+    // }
+
+    int hashing[256] = {0};
+    for (int i = 0; i < s.size(); i++)
+    {
+        hashing[s[i]]++;
+    }
+
+    int q;
+    cin >> q;
+
+    while (q--)
+    {
+        char c;
+        cin >> c;
+
+        // cout << hashing[c - 'a'] << endl;
+        cout << hashing[c] << endl;
+    }
+    return 0;
+}
